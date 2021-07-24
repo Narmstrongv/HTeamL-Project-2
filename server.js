a70ad7cb5f58dcf8d5bcc3588196830e5ce21c4e
Newton <narmstrongv@gmail.com>
2021-07-24 10:31:03 -0700

    added info/folder/server-testing

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 //basic server setup from 13.5 - needs to be tested
 
-// const express = require('express');
-// const routes = require('./routes');
-// const sequelize = require('./config/connection');
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
 
-// const app = express();
-// const PORT = process.env.PORT || 3001;
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// // turn on routes
-// app.use(routes);
+// turn on routes, comes from ./controller
+app.use(routes);
 
-// // turn on connection to db and server
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => console.log('Now listening'));
-// });
+// turn on connection to db and server
+//.sync means SEQ takes models & connects them to assoc. DB tables
+//force: true will drop & recreate all DB tables on startup
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
