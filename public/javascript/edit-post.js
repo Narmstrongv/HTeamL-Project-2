c2145b39f0e3fec318563329734fb8572ed11fff
Asher <asher.bergtraun@gmail.com>
2021-07-28 14:22:01 -0700

    added const and if statement to edit-post.js

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,6 +1,29 @@
 //create asynchronous function to capture edit post user input
 async function editFormHandler(event) {
     event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const leftover = document.querySelector('input[name="post-leftover"]').value;
+    const content = document.querySelector('input[name="post-content"]').value;
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title,
+            leftover,
+            content
+        }),
+        headers: { 'Content-Type': 'application/json'}
+    });
+
+    if (response.ok) {
+        document.location.replace('/profile/');
+    } else {
+        alert(response.statusText);
+    }
 }
 //actively listen for user input to run js
 document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
