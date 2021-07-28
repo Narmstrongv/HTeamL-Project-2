8a8bb44062d106a40aff9c97d7be8f7c34ebd0ef
Asher <asher.bergtraun@gmail.com>
2021-07-28 13:50:35 -0700

    started async function and event listener for create-post.js

---
+++ public/javascript/create-post.js
@@ -0,0 +1,6 @@
+//create asynchronous function to capture create post user input
+async function newFormHandler(event) {
+
+}
+//actively listen for user input to run js
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler)
