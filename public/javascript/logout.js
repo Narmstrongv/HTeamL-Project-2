0e304aebdf8ffce575e3f6fca1485105645df01c
Asher <asher.bergtraun@gmail.com>
2021-07-28 10:54:35 -0700

    started async function and event listener for logout.js

---
+++ public/javascript/logout.js
@@ -0,0 +1,6 @@
+//create asynchronous function to logout in response to user input
+async function logout() {
+
+}
+//actively listen for user input to run js
+document.querySelector('#logout').addEventListener('click', logout);
