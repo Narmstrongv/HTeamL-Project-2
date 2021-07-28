780e31baaeaaa515bb0bd801c297f2fc4d8d4ff6
Asher <asher.bergtraun@gmail.com>
2021-07-28 11:39:13 -0700

    started async function and event listener for signup.js

---
+++ public/javascript/signup.js
@@ -0,0 +1,6 @@
+//create asynchronous function to capture signup user input
+async function signupFormHandler(event) {
+
+}
+//actively listen for user input to run js
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler)
