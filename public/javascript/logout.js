3cbf32fc8bf3daeaf87ca218ff7cdd9b265f2a30
Asher <asher.bergtraun@gmail.com>
2021-07-28 11:11:05 -0700

    added response constant to logout.js

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,6 +1,9 @@
 //create asynchronous function to logout in response to user input
 async function logout() {
-
+    const response = await fetch('/api/users/logout', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json'}
+    });
 }
 //actively listen for user input to run js
 document.querySelector('#logout').addEventListener('click', logout);
