dd1925179d445448e25b82328496895d43164f3f
Asher <asher.bergtraun@gmail.com>
2021-07-28 11:45:50 -0700

    added constants to signup.js

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,5 +1,12 @@
 //create asynchronous function to capture signup user input
 async function signupFormHandler(event) {
+    event.preventDefault();
+    //identify user input and trim to make constants
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+    const twitter = document.querySelector('#twitter-signup').value.trim();
+    const github = document.querySelector('#github-signup').value.trim();
 
 }
 //actively listen for user input to run js
