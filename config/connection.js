08a86b02d8c7814fc0ed84e67c39f2600c2b1b28
Newton <narmstrongv@gmail.com>
2021-08-01 19:33:07 -0700

    heroku hotfix 1

--- config/connection.js
+++ config/connection.js
@@ -2,29 +2,29 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+//below may work now with JAWS defined
 
-//JAWS needed to be defined in .env in order to work
-if(process.env.JAWSDB_URL){
- const sequelize = new Sequelize(process.env.JAWSDB_URL);
+//if(process.env.JAWSDB_URL){
+ // const sequelize = new Sequelize(process.env.JAWSDB_URL);
  
-}else{
-  //create connection to our db
-  //use .env file for credentials
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
+//}else{
+  // create connection to our db
+// use .env file for credentials
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   port: 3306
   
-})};
+// });
 
-//const sequelize = new Sequelize(process.env.JAWSDB_URL);
+const sequelize = new Sequelize(process.env.JAWSDB_URL);
 
 //this gives extra log info
-// sequelize.authenticate().then(() => {
-//   console.log('Connection to database has been established successfully.');
-// }).catch(err => {
-//   console.error('Unable to connect to database:', err);
-// });
+sequelize.authenticate().then(() => {
+  console.log('Connection to database has been established successfully.');
+}).catch(err => {
+  console.error('Unable to connect to database:', err);
+});
 
 // }
 
