2f65e390bc4456352757d9b3d40806b87e72235e
Asher <asher.bergtraun@gmail.com>
2021-08-02 16:39:39 -0700

    resolved login route error

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
         include: [
           {
             model: User,
-            attributes: ['username']
+            attributes: ['username', 'twitter', 'github']
           }
         ]
       })
@@ -30,7 +30,7 @@ router.get('/:id', (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['username']
+          attributes: ['username', 'twitter', 'github']
         }
       ]
     })
--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
-
 const expressHandlebars = require('express-handlebars')
 const app = express();
 const PORT = process.env.PORT || 3003;
@@ -16,6 +15,24 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'bigdawg',
+    cookie: {
+          // Session will automatically expire in 10 minutes
+          expires: 20 * 60 * 1000
+    },
+    resave: true,
+    rolling: true,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    }),
+  };
+  
+  app.use(session(sess));
 
 // turn on routes, comes from ./controller
 app.use(routes);
