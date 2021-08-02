dbdfcdc7a4916c3116f7f341e9891ae834050c58
Newton <narmstrongv@gmail.com>
2021-08-01 19:03:21 -0700

    Testing new connection params

--- config/connection.js
+++ config/connection.js
@@ -8,12 +8,13 @@ require('dotenv').config();
 //}else{
   // create connection to our db
 // use .env file for credentials
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   port: 3306
   
-});
+// });
+const sequelize = new Sequelize('mysql://d4xa9m60m6ezu09z:ft7n8rqzlu855ylh@ckshdphy86qnz0bj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/tzjuljd1cxf2clud')
 
 sequelize.authenticate().then(() => {
   console.log('Connection to database has been established successfully.');
