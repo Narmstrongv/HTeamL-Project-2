4962b8d14f938a4af6d7bc54869620dd762f035d
Newton <narmstrongv@gmail.com>
2021-08-02 00:04:57 -0700

    added static public folder

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 //basic server setup from 13.1.6
 //server opens on http://localhost:3003/api/etc.
 
+const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+
 
 const expressHandlebars = require('express-handlebars')
 
@@ -12,8 +14,10 @@ const PORT = process.env.PORT || 3003;
 
 app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes, comes from ./controller
 app.use(routes);
