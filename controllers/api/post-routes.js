e8d527f60a3da57c9c8afbd2e593eaf1d1797c56
Asher <asher.bergtraun@gmail.com>
2021-08-02 17:19:55 -0700

    troubleshooting profile route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,7 +4,7 @@ const {Post, User}= require('../../models');
 router.get('/', (req, res) => {
 
     Post.findAll({
-        attributes: ['id', 'title', 'body', 'created_at'],
+        attributes: ['id', 'title', 'body', 'user_id'],
         order: [['created_at', 'DESC']], 
         include: [
           {
@@ -26,7 +26,7 @@ router.get('/:id', (req, res) => {
       where: {
         id: req.params.id
       },
-      attributes: ['id', , 'title', 'body', 'created_at'],
+      attributes: ['id', , 'title', 'body', 'user_id'],
       include: [
         {
           model: User,
